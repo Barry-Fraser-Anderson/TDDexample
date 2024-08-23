ccf1549e9a161109cf2bbb667f6ae8d7a28d8fce
Barry Anderson <barryanderson60@outlook.com>
2024-08-23 17:50:03 +0100

    TwoNumber test pass

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,7 +6,14 @@
         {
             if (string.IsNullOrEmpty(value)) return 0;
 
-            return int.Parse(value);
+            int commaPos = value.IndexOf(',');
+            if (commaPos == -1)
+                return int.Parse(value);
+
+            string num1 = value.Substring(0, commaPos);
+            string num2 = value.Substring(commaPos + 1);
+
+            return int.Parse(num1) + int.Parse(num2);
         }
     }
 }
