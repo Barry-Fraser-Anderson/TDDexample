2359e0d4871846136b7cfab8627de33c37108f4e
Barry Anderson <barryanderson60@outlook.com>
2024-08-23 18:42:04 +0100

    NewDelim test pass

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,16 +6,19 @@
         {
             if (string.IsNullOrEmpty(value)) return 0;
 
-            int commaPos = value.IndexOf(',');
-            if (commaPos == -1)
-                return int.Parse(value);
-
             string[] lines = value.Split('\n');
+
+            char delim = ',';
+            if ((lines[0].Length == 3) && lines[0].StartsWith("//"))
+            {
+                delim = lines[0][2];
+                lines[0] = "0";
+            }
 
             int total = 0;
             foreach (string line in lines)
             {
-                string[] numbers = line.Split(',');
+                string[] numbers = line.Split(delim);
                 foreach (string number in numbers)
                 {
                     total += int.Parse(number);
