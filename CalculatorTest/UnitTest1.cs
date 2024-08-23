2d5ff506cdf258434dfec104dce484043c04f239
Barry Anderson <barryanderson60@outlook.com>
2024-08-23 16:31:37 +0100

    Implement EmptyString test

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,11 +1,21 @@
+using FluentAssertions;
+using System.ComponentModel;
+
 namespace CalculatorTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Calculator_EmptyString_ReturnZero()
         {
+            var result = Add("");
 
+            result.Should().Be(0); 
+        }
+
+        int Add(string value)
+        {
+            throw new NotImplementedException();
         }
     }
 }
